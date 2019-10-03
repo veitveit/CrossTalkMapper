cfc6ab2a44a5e4e622d04864020c4131a5eebb0d
Rebecca Kirsch <rebecca_kirsch@gmx.de>
2019-10-03 10:19:53 +0200

    Add data/, update README.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CrossTalkMapper
+
+## Description of repository content
+
+### Data
+
+Contains the example dataset downloaded from CrosstalkDB, including an auxiliary file for data preparation and a detailed description in the README.
---
+++ data/map-timepoints2CrDB.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+
+# input: CrosstalkDB output csv containing all datasets from mouse tissue study (Tvardovskiy 2017), with header
+# maps time points and number of the biological replicate to CrDB identifiers
+# output: CrosstalkDB output csv with additional columns for time points and the biological replicate numbers, including header
+
+use strict;
+use warnings;
+use Data::Dumper; $Data::Dumper::Sortkeys = 1;
+
+# create list of tissues
+
+my @tissues = ("brain", "heart", "liver", "kidney");
+
+# create list of timepoints
+
+my @timepoints = (("3 months") x 4, ("5 months") x 4, ("10 months") x 4, ("18 months") x 2, ("24 months") x 2);
+
+# create list of replicates
+
+my @reps = ((1..4) x 3, (1..2) x 2);
+
+# create hash of CrDB ids and time points
+
+my %map;
+my $tissue_counter = my $timerep_counter = 0;
+
+for (my $crdb_nr = 62; $crdb_nr <= 129; $crdb_nr++) {
+
+	next if $crdb_nr == 64 || $crdb_nr == 65 || $crdb_nr == 66 || $crdb_nr == 67;
+
+	my $tissue = $tissues[$tissue_counter];
+	my $time = $timepoints[$timerep_counter];
+	my $rep = $reps[$timerep_counter];
+
+	my $leadzero = sprintf("%06d", $crdb_nr);
+	my $crdb = "CrDB$leadzero";
+
+	$map{$crdb} = "$tissue,$time,$rep";
+
+	$timerep_counter++;
+
+	if ($crdb_nr == 81 || $crdb_nr == 97 || $crdb_nr == 113) {
+
+		$tissue_counter++;
+		$timerep_counter = 0;
+
+	}
+
+}
+
+# read input data and add time point and biological replicate
+
+my $incsv = $ARGV[0];
+open(my $in_fh, '<', $incsv) or die("Could not open $incsv, exiting $!");
+
+while (my $inline = <$in_fh>) {
+
+	chomp($inline);
+	my $out;	
+
+	if ($inline =~ /accession number/) {
+
+		$out = "$inline,\"timepoint\",\"biological replicate\"";
+
+	} else {
+
+		$inline =~ /(CrDB\d+)\"$/;
+		my $crdb = $1;
+
+		my ($tissue, $time, $rep) = split(',', $map{$crdb});
+
+		my $info = $map{$crdb};
+
+		$out = "$inline,\"$time\",\"$rep\"";
+
+	}
+
+	print STDOUT "$out\n";
+
+}
+
+chomp(my @incsv = <$in_fh>);
+close($in_fh);
